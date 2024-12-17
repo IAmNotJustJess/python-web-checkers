720d34d1f931a1f0b0fd2caddc3493e0db4a420d
Jessie <banderegewigz@gmail.com>
2024-12-17 19:27:54 +0100

    sounds and rotation yay!!!

--- sourcefiles/client.py
+++ sourcefiles/client.py
@@ -1,5 +1,7 @@
 # Example file showing a circle moving on screen
 import pygame
+import pathlib
+import random
 from checkers import Checkers
 from connection import Connection
 
@@ -18,16 +20,43 @@ FILL_RED = (160, 120, 120)
 FILL_BLACK = (100, 100, 100)
 FILL_NEUTRAL = (120, 120, 120)
 
-# pygame setup
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("PyGame Web Checkers", icontitle="")
 clock = pygame.time.Clock()
 dt = 0
 
-pygame.font.init() # you have to call this at the start, 
-                   # if you want to use this module.
+pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 24)
+
+pygame.mixer.init()
+soundlist = [
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-1.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-2.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-3.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-4.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-5.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-6.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-7.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-8.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-9.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-10.wav"))),
+    pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("piece-11.wav")))
+    ]
+
+victory_sfx = pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("victory.mp3")))
+defeat_sfx = pygame.mixer.Sound(str(pathlib.Path(__file__).parent.resolve().joinpath("sfx").joinpath("defeat.mp3")))
+
+for i in range(0, 11):
+    pygame.mixer.Sound.set_volume(soundlist[i], 0.12)
+pygame.mixer.Sound.set_volume(victory_sfx, 0.12)
+pygame.mixer.Sound.set_volume(defeat_sfx, 0.12)
+
+def flippedBoard(board):
+    new = board[::-1]
+    for x in range(0, 8):
+        new[x] = new[x][::-1]
+    return new
 
 def drawGrid(game, player):
     
@@ -39,27 +68,41 @@ def drawGrid(game, player):
     else:
         if(game.changed == 1):
             return
+        board = game.checkers.copy()
+        selected_grid = game.selected_grid.copy()
+        allow_grid = game.allow_grid.copy()
+        compulsory_tiles = game.compulsory_tiles.copy()
+        if player == 'b':
+            board = flippedBoard(board)
+            for i in range(0, len(selected_grid)):
+                if selected_grid[i] != [-1, -1]:
+                    selected_grid[i] = [7 - selected_grid[i][0], 7 - selected_grid[i][1]]
+            for i in range(0, len(allow_grid)):
+                if allow_grid[i] != [-1, -1]:
+                    allow_grid[i] = [7 - allow_grid[i][0], 7 - allow_grid[i][1]]
+            for i in range(0, len(compulsory_tiles)):
+                if compulsory_tiles[i] != [-1, -1]:
+                    compulsory_tiles[i] = [7 - compulsory_tiles[i][0], 7 - compulsory_tiles[i][1]]
         screen.fill(FILL_RED if player == 'r' else FILL_BLACK)
-        game.changed == 1
         blockSize = 64
         rect = pygame.Rect(86, 86, 544, 544)
         pygame.draw.rect(screen, BLACK_DARKER, rect)
         for x in range(0, 8):
             for y in range(0, 8):
                 rect = pygame.Rect((102 + 64 * x), (102 + 64 * y), blockSize, blockSize)
-                if(game.selected_grid[0] == [y, x] or game.selected_grid[1] == [y, x]):
+                if([y, x] in selected_grid):
                     pygame.draw.rect(screen, SELECTED, rect)
-                elif(game.allow_grid[0] == [y, x] or game.allow_grid[1] == [y, x] or game.allow_grid[2] == [y, x] or game.allow_grid[3] == [y, x]):
+                elif([y, x] in allow_grid):
                     pygame.draw.rect(screen, ALLOW, rect)
-                elif([y, x] in game.compulsory_tiles):
+                elif([y, x] in compulsory_tiles):
                     pygame.draw.rect(screen, COMPULSORY, rect)
                 else:
                     pygame.draw.rect(screen, WHITE if (x + y) % 2 == 0 else BLACK, rect)
-                if(game.checkers[y][x][0] == "r"):
+                if(board[y][x][0] == "r"):
                     pygame.draw.circle(screen, RED_PIECE, pygame.Vector2((134 + 64 * x), (134 + 64 * y)), 24)
-                if(game.checkers[y][x][0] == "b"):
+                if(board[y][x][0] == "b"):
                     pygame.draw.circle(screen, BLACK_PIECE, pygame.Vector2((134 + 64 * x), (134 + 64 * y)), 24)
-                if(game.checkers[y][x] == "rq" or game.checkers[y][x] == "bq"):
+                if(board[y][x] == "rq" or board[y][x] == "bq"):
                     pygame.draw.circle(screen, GOLD, pygame.Vector2((134 + 64 * x), (134 + 64 * y)), 24, 4)
         if(game.current_turn == "r" and game.compulsory_moves_red == False): text_surface = my_font.render("Red's turn.", True, RED_PIECE)
         if(game.current_turn == "r" and game.compulsory_moves_red == True): text_surface = my_font.render("Red's turn - compulsory take!", True, RED_PIECE)
@@ -84,15 +127,25 @@ def main():
     changeOccoured = False
     game = Checkers(0)
     while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         try:
             screen.fill((120, 120, 120))
             if(receivedAlready == False):
                 game = n.send("get")
                 if((game.connected() == True and game.current_turn == player) or (game.running == False)):
+                    if game.running == False:
+                        if player == 'b':
+                            if(game.any_moves_available_black == False or game.black_pieces <= 0):
+                                defeat_sfx.play()
+                            if(game.any_moves_available_red == False or game.red_pieces <= 0):
+                                victory_sfx.play()
+                        else:
+                            if(game.any_moves_available_black == False or game.black_pieces <= 0):
+                                victory_sfx.play()
+                            if(game.any_moves_available_red == False or game.red_pieces <= 0):
+                                defeat_sfx.play()
+                    else:
+                        soundlist[random.randint(0, 10)].play()
                     receivedAlready = True
-            drawGrid(game, player)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     running = False
@@ -101,25 +154,24 @@ def main():
                     y = pygame.mouse.get_pos()[0]
                     x = (x - 102) // 64
                     y = (y - 102) // 64
+                    if player == 'b':
+                        x = 7 - x
+                        y = 7 - y
                     changeOccoured = game.updateSelection(x, y)
                     if(changeOccoured == True):
+                        soundlist[random.randint(0, 10)].play()
                         data = "map:"+''.join(str(x) for x in game.checkers)+''.join(str(x) for x in game.last_move)
                         n.send(data)
                         receivedAlready = False
                         changeOccoured = False
+            drawGrid(game, player)
         except:
             running = False
             print("Couldn't get the game instance.")
             break
 
-        # fill the screen with a color to wipe away anything from last frame
-
-        # flip() the display to put your work on screen
         pygame.display.flip()
 
-        # limits FPS to 60
-        # dt is delta time in seconds since last frame, used for framerate-
-        # independent physics.
         dt = clock.tick(120) / 1000
 
 main()
